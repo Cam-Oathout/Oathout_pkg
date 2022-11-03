533e55b4dbd425a5316264fbce5351005de7ee92
Camden Oathout <camden.oathout@selu.edu>
2022-11-03 18:56:30 +0000

    Actual Data cleaning function

--- Scripts/data_clean_func.R
+++ Scripts/data_clean_func.R
@@ -1,1 +1,10 @@
 #Data Clean-Up Function 
+
+cleandata <- function(filepath){
+  data_raw <- read_csv(filepath)
+  clean <- na.omit(data_raw)
+  check <- anyNA(clean)
+  return(check) 
+}
+
+cleandata("Data/eDNA_data.csv")
