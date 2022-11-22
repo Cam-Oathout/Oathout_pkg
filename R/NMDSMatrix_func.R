f5ce35b093592fa92853f65f176d0a43fc4127bb
Camden Oathout <camden.oathout@selu.edu>
2022-11-22 21:12:01 +0000

    Creating a matrix to use for NMDS analysis from your data frame.

--- R/NMDSMatrix_func.R
+++ R/NMDSMatrix_func.R
@@ -1,8 +1,21 @@
-#' Creating an NMDS Matrix Function: Creating a matrix from your dataframe to make an NMDS plot
+#' Creating an NMDS Matrix Function: Creating a matrix from your data frame to make an NMDS plot
 #' 
-#' @param 
+#' @param data = object you named your data when you read it in
+#' @param col1 = column from the data frame you want to use in your NMDS analysis, such as a column listing the names of your detected species
+#' @param col2 = a different column from the data frame you want to use in your NMDS analysis, such as a count of your detected species
+#' @param col3 = a different column from the others in the data frame you want to use in your NMDS analysis, such as different habitats or other relevent differences
 #' 
-#' @return 
+#' @return matrix showing species counts for different habitats or other relevent differences  
 #' 
 #' @examples 
+#' NMDSmatrix(data, col1, col2, col3)
+#' 
 #'  
+NMDSmatrix <- function(data, col1, col2, col3) {
+     speciesmatrix <- data %>% 
+           select({{col1}}, {{col2}}, {{col3}}) %>% 
+           group_by({{col2}}, {{col3}}) %>% 
+           spread({{col2}}, {{col3}}) %>% 
+           data.matrix(rownames.force = data$col2)
+     return(speciesmatrix)
+ } 
